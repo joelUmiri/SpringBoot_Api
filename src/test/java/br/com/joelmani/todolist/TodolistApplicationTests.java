84797eb801d43ab3ff44cdb422ef8d2865da68f3
Joel <joelm4mani@gmail.com>
2026-02-12 12:45:51 -0300

    Utilização do response entity ok

---
+++ src/test/java/br/com/joelmani/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelmani.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
