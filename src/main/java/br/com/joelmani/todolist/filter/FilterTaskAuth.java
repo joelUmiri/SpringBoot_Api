946c9c1c1981e15519ecfcfbb14f10e0c869bc3f
Joel <joelm4mani@gmail.com>
2026-02-18 21:28:08 -0300

    Divisão de usuário e senha concluído

---
+++ src/main/java/br/com/joelmani/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,50 @@
+package br.com.joelmani.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+                //pegar a autenticação (Usuário e senha)
+                var authorization = request.getHeader("Authorization");
+                
+                var authEncoded = authorization.substring("basic".length()).trim();
+
+                byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+                var authString =  new String(authDecoded);
+                
+                //["Dazai", "m75r4v8d1#"]
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+                
+                System.out.println("Authorization");
+                System.out.println(username);
+                System.out.println(password);
+                //validar usuario
+                
+                //validar senha
+
+                //segue viagem
+
+                filterChain.doFilter(request, response);
+    }
+
+    
+    
+}
