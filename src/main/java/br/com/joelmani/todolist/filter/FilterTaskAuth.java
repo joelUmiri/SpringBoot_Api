0b1effb4e6c641571b4bfe32262ed9c01e940f74
Joel <joelm4mani@gmail.com>
2026-02-19 10:51:17 -0300

    validação de rota concluída

--- src/main/java/br/com/joelmani/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/joelmani/todolist/filter/FilterTaskAuth.java
@@ -3,48 +3,63 @@ package br.com.joelmani.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.joelmani.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                //pegar a autenticação (Usuário e senha)
-                var authorization = request.getHeader("Authorization");
-                
-                var authEncoded = authorization.substring("basic".length()).trim();
+        var servletPath = request.getServletPath();
+        if (servletPath.equals("/tasks/")) {
+            // pegar a autenticação (Usuário e senha)
+            var authorization = request.getHeader("Authorization");
 
-                byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+            var authEncoded = authorization.substring("basic".length()).trim();
 
-                var authString =  new String(authDecoded);
-                
-                //["Dazai", "m75r4v8d1#"]
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
-                
-                System.out.println("Authorization");
-                System.out.println(username);
-                System.out.println(password);
-                //validar usuario
-                
-                //validar senha
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
 
-                //segue viagem
+            var authString = new String(authDecoded);
 
-                filterChain.doFilter(request, response);
+            // ["Dazai", "m75r4v8d1#"]
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+            // validar usuario
+
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+
+                // validar senha
+
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+                // segue viagem
+
+            }
+        }else {
+            filterChain.doFilter(request, response);
+        }
     }
 
-    
-    
 }
